f4e82d7184e34beb9040d12d5f057eaab85e9f1f
Andy Nur <andylulz@yahoo.com>
2020-02-07 15:14:49 +0800

    print template

--- resources/views/survey/print.blade.php
+++ resources/views/survey/print.blade.php
@@ -5,19 +5,38 @@
     <style>
         h1, h2, h3, h4, h5, h6, p {
             margin: 0;
+            font-weight: normal;
         }
         body {
             max-width: 768px;
         }
+        table {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        h5 {
+            font-size: .9rem;
+        }
+
+        h4 {
+            font-size: 1.2rem;
+        }
+
+        td {
+            border: 1px solid #000;
+            padding: 6px;
+            font-size: .8rem;
+        }
     </style>
 </head>
 <body>
-    <table border="1">
-        <tr>
-            <td style="width: 50px;">
-                <img src="{{ asset('images/logo.png') }}" alt="logo bpn">
+    <table style="width: 100%;">
+        <tr style="text-align: center;">
+            <td style="width: 40px; border-right: 0;">
+                <img src="{{ asset('images/logo.png') }}" alt="logo bpn" style="width: 100%; margin-left: 10px;">
             </td>
-            <td>
+            <td style="width: 260px; border-left: 0;">
                 <h5>PEMERINTAH KOTA BALIKPAPAN</h5>
                 <h4>DINAS PEKERJAAN UMUM</h4>
                 <h5>BIDANG GEDUNG PEMERINTAH</h5>
@@ -25,8 +44,8 @@
             <td>
                 <h5>KODE BANGUNAN<br>{{ $data->kode }}</h5>
             </td>
-            <td>
-                <p>{{ $data->nama_gedung }}</p>
+            <td style="text-align: left">
+                <p style="margin-bottom: .5rem;">{{ $data->nama_gedung }}</p>
                 <p>{{ $data->tanggal }}</p>
             </td>
         </tr>
@@ -36,12 +55,62 @@
             </td>
         </tr>
         <tr>
-            <td colspan="3">
+            <td style="height: 400px" colspan="4"></td>
+        </tr>
+        <tr>
+            <td colspan="3" style="border-right: 0;">
                 <h5>II. DATA TEKNIS</h5>
             </td>
-            <td>
+            <td style="border-left: 0;">
                 <h5>TAHUN ANGGARAN : 0000</h5>
             </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="padding-left: 1.4rem;">
+                1. CARA PELAKSANAAN
+            </td>
+            <td colspan="2">
+                [] PERUSAHAAN
+                [] SWAKELOLA
+            </td>
+        </tr>
+        <tr>
+            <table style="width: 100%;">
+                <tr>
+                    <td colspan="3" style="width: 618px; padding-left: 1.4rem;">2. DATA PELAKSANAAN</td>
+                    <td>NAMA REKANAN</td>
+                    <td>NILAI KONTRAK</td>
+                    <td>KUALIFIKASI</td>
+                    <td>JANGKA WAKTU</td>
+                </tr>
+                <tr style="height: 80px;">
+                    <td style="width: 20px">&nbsp;</td>
+                    <td style="width: 133px; text-align: center;">A</td>
+                    <td style="width: 133px;">&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr style="height: 80px;">
+                    <td>&nbsp;</td>
+                    <td style=" text-align: center;">B</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr style="height: 80px;">
+                    <td>&nbsp;</td>
+                    <td style="text-align: center;">C</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
         </tr>
     </table>
 </body>
