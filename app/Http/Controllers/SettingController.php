407036b3f97fa4adfb73c0d8ae5def4906b0d124
Andy Nur <andylulz@yahoo.com>
2020-02-07 13:45:44 +0800

    profile update

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Setting;
+use App\User;
+use Auth;
+use Hash;
 
 class SettingController extends Controller
 {
@@ -19,6 +22,38 @@ class SettingController extends Controller
         $setting = Setting::findOrFail($id);
         $setting->update($request->all());
 
-        return back()->with('success', 'Berhasil merubah pengaturan aplikasi');
+        return back()->with('success', 'Berhasil mengubah pengaturan aplikasi');
+    }
+
+    public function profile()
+    {
+        $uid  = Auth::user()->id;
+        $user = User::findOrFail($uid);
+
+        return view('setting.profile', compact('user'));
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $uid  = Auth::user()->id;
+        $user = User::findOrFail($uid);
+
+        if ($request->has('old_password')) {
+            if (! Hash::check($request->old_password, $user->password)) {
+                return back()->with('error', 'Password lama salah.');
+            }
+
+            if ($request->password !== $request->password_confirmation) {
+                return back()->with('error', 'Konfirmasi password baru tidak sama.');
+            }
+
+            $user->update(['password' => Hash::make($request->password)]);
+
+            return back()->with('success', 'Berhasil mengubah password pengguna');
+        }
+
+        $user->update($request->all());
+
+        return back()->with('success', 'Berhasil mengubah profil pengguna');
     }
 }
