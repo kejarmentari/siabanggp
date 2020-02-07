da39896585965c69993e67a1a0b0393f415e222b
Andy Nur <andylulz@yahoo.com>
2020-02-07 15:49:51 +0800

    update font size

--- resources/views/survey/show.blade.php
+++ resources/views/survey/show.blade.php
@@ -7,19 +7,19 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-3 col-12 mt-2">
-                        <h5>Kode Bangunan</h5>
+                        <h6>Kode Bangunan</h6>
                     </div>
                     <div class="col-md-9 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->kode}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->kode}}</h6>
                     </div>
                     <div class="col-md-3 col-12 mt-2">
-                        <h5>Tanggal</h5>
+                        <h6>Tanggal</h6>
                     </div>
                     <div class="col-md-9 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->tanggal}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->tanggal}}</h6>
                     </div>
                     <div class="col-md-3 col-12 mt-2">
-                        <h5>Foto / Video:</h5>
+                        <h6>Foto / Video</h6>
                     </div>
                     <div class="col-md-4 col-12 mt-2">
                         <img class="img-thumbnail" src="{{$survey->foto}}" />
@@ -33,144 +33,144 @@
 
                 <hr class="mt-4">
 
-                <h2 class="mt-4">I. DATA UMUM</h2>
+                <h3 class="mt-4">I. DATA UMUM</h3>
                 <div class="row no-gutters mt-3">
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>1. Nama Pemilik Bangunan Gedung</h5>
+                        <h6>1. Nama Pemilik Bangunan Gedung</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->pemilik_gedung}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->pemilik_gedung}}</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>2. Nama Bangunan Gedung</h5>
+                        <h6>2. Nama Bangunan Gedung</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->nama_gedung}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->nama_gedung}}</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>3. Fungsi Bangunan Gedung</h5>
+                        <h6>3. Fungsi Bangunan Gedung</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->fungsi_gedung == null ? '-' : $survey->fungsi_gedung}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->fungsi_gedung == null ? '-' : $survey->fungsi_gedung}}</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>4. Klasifikasi bangunan gedung</h5>
+                        <h6>4. Klasifikasi bangunan gedung</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->klasifikasi_gedung == null ? '-' : $survey->klasifikasi_gedung}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->klasifikasi_gedung == null ? '-' : $survey->klasifikasi_gedung}}</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>5. Lokasi bangunan gedung</h5>
+                        <h6>5. Lokasi bangunan gedung</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->lokasi_gedung == null ? '-' : $survey->lokasi_gedung}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->lokasi_gedung == null ? '-' : $survey->lokasi_gedung}}</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>6. Jumlah lantai bangunan gedung</h5>
+                        <h6>6. Jumlah lantai bangunan gedung</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->jumlah_lantai}} Lantai</h5>
+                        <h6 class="font-weight-normal">: {{$survey->jumlah_lantai}} Lantai</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>7. Ketinggian bangunan</h5>
+                        <h6>7. Ketinggian bangunan</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->ketinggian}} Meter</h5>
+                        <h6 class="font-weight-normal">: {{$survey->ketinggian}} Meter</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>8. Jumlah lantai bangunan gedung</h5>
+                        <h6>8. Jumlah lantai bangunan gedung</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->luas_lantai}} Meter Persegi</h5>
+                        <h6 class="font-weight-normal">: {{$survey->luas_lantai}} Meter Persegi</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>9. Luas lantai dasar bangunan gedung</h5>
+                        <h6>9. Luas lantai dasar bangunan gedung</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->luas_lantai_dasar}} Meter Persegi</h5>
+                        <h6 class="font-weight-normal">: {{$survey->luas_lantai_dasar}} Meter Persegi</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>10. Luas lantai</h5>
+                        <h6>10. Luas lantai</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->luas_tanah}} Meter Persegi</h5>
+                        <h6 class="font-weight-normal">: {{$survey->luas_tanah}} Meter Persegi</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>11.  Posisi Koordinat (X,Y)</h5>
+                        <h6>11.  Posisi Koordinat (X,Y)</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->latitude}}, {{$survey->longitude}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->latitude}}, {{$survey->longitude}}</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>12.  Surat bukti atas hak tanah</h5>
+                        <h6>12.  Surat bukti atas hak tanah</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
                         @if ($survey->surat_bukti == 1)
-                            <h5 class="font-weight-normal">: Sertifikat</h5>
+                            <h6 class="font-weight-normal">: Sertifikat</h6>
                         @elseif($survey->surat_bukti == 2)
-                        <h5 class="font-weight-normal">: IMTN</h5>
+                        <h6 class="font-weight-normal">: IMTN</h6>
                         @else
-                            <h5 class="font-weight-normal">: Bukti Lainnya</h5>
+                            <h6 class="font-weight-normal">: Bukti Lainnya</h6>
                         @endif
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>13. Nama PPTK</h5>
+                        <h6>13. Nama PPTK</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->pptk == null ? '-' : $survey->pptk}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->pptk == null ? '-' : $survey->pptk}}</h6>
                     </div>
 
                     <div class="col-md-4 col-12 mt-2">
-                        <h5>14. Nama PPK</h5>
+                        <h6>14. Nama PPK</h6>
                     </div>
                     <div class="col-md-8 col-12 mt-2">
-                        <h5 class="font-weight-normal">: {{$survey->ppk == null ? '-' : $survey->ppk}}</h5>
+                        <h6 class="font-weight-normal">: {{$survey->ppk == null ? '-' : $survey->ppk}}</h6>
                     </div>
 
                     <div class="col-12">
-                        <h2 class="mt-4">II. DATA TEKNIS</h2>
+                        <h3 class="mt-4">II. DATA TEKNIS</h3>
                     </div>
                         <div class="col-md-4 col-12 mt-2">
-                            <h5>1. Tahun anggaran</h5>
+                            <h6>1. Tahun anggaran</h6>
                         </div>
                         <div class="col-md-8 col-12 mt-2">
-                            <h5 class="font-weight-normal">: {{$survey->tahun_anggaran == null ? '-' : $survey->tahun_anggaran}}</h5>
+                            <h6 class="font-weight-normal">: {{$survey->tahun_anggaran == null ? '-' : $survey->tahun_anggaran}}</h6>
                         </div>
                         <div class="col-md-4 col-12 mt-2">
-                            <h5>2. Cara pelaksanaan</h5>
+                            <h6>2. Cara pelaksanaan</h6>
                         </div>
                         <div class="col-md-8 col-12 mt-2">
                             @if ($survey->cara_pelaksanaan == 0)
-                                <h5 class="font-weight-normal">: Swasta</h5>
+                                <h6 class="font-weight-normal">: Swasta</h6>
                             @else
-                                <h5 class="font-weight-normal">: Perusahaan</h5>
+                                <h6 class="font-weight-normal">: Perusahaan</h6>
                             @endif
                         </div>
                     <div class="col-12">
-                        <h2 class="mt-4">A. DATA ADMINISTRASI</h2>
+                        <h3 class="mt-4">A. DATA ADMINISTRASI</h3>
                         <div class="row">
                             @foreach ($survey_selections as $selection)
                             @if ($selection->selection->type == 1)
                                 <div class="col-md-4 col-12 mt-2">
-                                    <h5>{{$selection->selection->title}}</h5>
+                                    <h6>{{$selection->selection->title}}</h6>
                                 </div>
                                 <div class="col-md-8 col-12 mt-2">
                                     @if ($selection->choice == 0)
-                                        <h5 class="font-weight-normal">: Tidak Sesuai</h5>
+                                        <h6 class="font-weight-normal">: Tidak Sesuai</h6>
                                     @else
-                                        <h5 class="font-weight-normal">: Sesuai</h5>
+                                        <h6 class="font-weight-normal">: Sesuai</h6>
                                     @endif
 
                                 </div>
@@ -178,18 +178,18 @@
                         @endforeach
                         </div>
 
-                        <h2 class="mt-4">B. DATA KONSTRUKSI</h2>
+                        <h3 class="mt-4">B. DATA KONSTRUKSI</h3>
                         <div class="row">
                             @foreach ($survey_selections as $selection)
                             @if ($selection->selection->type == 2)
                                 <div class="col-md-4 col-12 mt-2">
-                                    <h5>{{$selection->selection->title}}</h5>
+                                    <h6>{{$selection->selection->title}}</h6>
                                 </div>
                                 <div class="col-md-8 col-12 mt-2">
                                     @if ($selection->choice == 0)
-                                        <h5 class="font-weight-normal">: Tidak Sesuai</h5>
+                                        <h6 class="font-weight-normal">: Tidak Sesuai</h6>
                                     @else
-                                        <h5 class="font-weight-normal">: Sesuai</h5>
+                                        <h6 class="font-weight-normal">: Sesuai</h6>
                                     @endif
 
                                 </div>
@@ -197,18 +197,18 @@
                         @endforeach
                         </div>
 
-                        <h2 class="mt-4">C. DATA PEMELIHARAAN</h2>
+                        <h3 class="mt-4">C. DATA PEMELIHARAAN</h3>
                         <div class="row">
                             @foreach ($survey_selections as $selection)
                             @if ($selection->selection->type == 3)
                                 <div class="col-md-4 col-12 mt-2">
-                                    <h5>{{$selection->selection->title}}</h5>
+                                    <h6>{{$selection->selection->title}}</h6>
                                 </div>
                                 <div class="col-md-8 col-12 mt-2">
                                     @if ($selection->choice == 0)
-                                        <h5 class="font-weight-normal">: Tidak Sesuai</h5>
+                                        <h6 class="font-weight-normal">: Tidak Sesuai</h6>
                                     @else
-                                        <h5 class="font-weight-normal">: Sesuai</h5>
+                                        <h6 class="font-weight-normal">: Sesuai</h6>
                                     @endif
 
                                 </div>
