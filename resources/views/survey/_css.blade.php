3ee4721d0543c13e8d08b0c3b40f5daa70bfb299
Andy Nur <andylulz@yahoo.com>
2020-02-08 18:29:59 +0800

    survey delete

---
+++ resources/views/survey/_css.blade.php
@@ -0,0 +1,31 @@
+<style>
+    #mapid {
+        height: 200px;
+        width: 100%;
+    }
+    div.dataTables_wrapper div.dataTables_length label {
+        margin-top: 9px;
+    }
+    table tbody  tr {
+        cursor: pointer;
+    }
+    table tbody tr.selected {
+        background: #198ae3!important;
+        color: #fff;
+    }
+    table tbody tr:not(.selected):hover {
+        background:  #198ae3;
+        filter: opacity(.7);
+        color: #fff;
+    }
+    select.form-control {
+        padding: 0 1.5rem 0 .5rem;
+    }
+    div.dataTables_wrapper div.dataTables_paginate {
+        margin-top: 1rem;
+    }
+    div.dataTables_wrapper div.dataTables_info {
+        padding-top: 1.5em;
+        font-size: .9rem;
+    }
+</style>
---
+++ resources/views/survey/_js.blade.php
@@ -0,0 +1,78 @@
+<script>
+$(function () {
+    // init map view
+    var map = L.map('mapid').setView([$('table tbody tr.selected').data('lat'), $('table tbody tr.selected').data('long')], {{ config('leaflet.detail_zoom_level') }});
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    L.marker([$('table tbody tr.selected').data('lat'),$('table tbody tr.selected').data('long')]).addTo(map)
+        .bindPopup('');
+
+    // init datatable with custom language
+    $('.table').DataTable({
+        "language": {
+            "sProcessing": "Sedang proses...",
+            "sLengthMenu": "Tampilan _MENU_ entri",
+            "sZeroRecords": "Tidak ditemukan data yang sesuai",
+            "sInfo": "Tampilan _START_ sampai _END_ dari _TOTAL_ entri",
+            "sInfoEmpty": "Tampilan 0 hingga 0 dari 0 entri",
+            "sInfoFiltered": "(disaring dari _MAX_ entri keseluruhan)",
+            "sInfoPostFix": "",
+            "sSearch": "Cari:",
+            "sUrl": "",
+            "oPaginate": {
+                "sFirst": "<i class='mdi mdi-chevron-double-left'></i>",
+                "sPrevious": "<i class='mdi mdi-arrow-left'></i>",
+                "sNext": "<i class='mdi mdi-arrow-right'></i>",
+                "sLast": "<i class='mdi mdi-chevron-double-right'></i>"
+            }
+        }
+    });
+
+    // row click => change map view focus
+    $('table tbody tr').on('click', function() {
+        $(this).siblings('tr').removeClass('selected');
+        $(this).addClass('selected');
+        $('.selected-lat').html($(this).data('lat'));
+        $('.selected-long').html($(this).data('long'));
+        map.setView([$(this).data('lat'), $(this).data('long')], {{ config('leaflet.detail_zoom_level') }})
+        L.marker([$(this).data('lat'), $(this).data('long')]).addTo(map)
+        .bindPopup('');
+    })
+
+    // set default lat & long
+    $('.selected-lat').html($('table tbody tr.selected').data('lat'));
+    $('.selected-long').html($('table tbody tr.selected').data('long'));
+
+    // detail click => go to detail page
+    $('.detail-button').on('click', function(e) {
+        e.preventDefault();
+        $(location).attr('href',`/survey/${$('table tbody tr.selected').data('id')}`);
+    })
+
+    // edit click => go to edit page
+    $('.edit-button').on('click', function(e) {
+        e.preventDefault();
+        $(location).attr('href',`/survey/edit/${$('table tbody tr.selected').data('id')}`);
+    })
+
+    // print click => go to print page
+    $('.cetak-button').on('click', function(e) {
+        e.preventDefault();
+        $(location).attr('href',`/cetak/${$('table tbody tr.selected').data('id')}`);
+    })
+
+    // remove button => confirm alert
+    $('.hapus-button').on('click', function(e) {
+        e.preventDefault();
+        const message = "Yakin ingin menghapus data gedung?"
+        const confirmAlert = confirm(message);
+        if (confirmAlert == true) {
+            const id = $('table tbody tr.selected').data('id')
+            const $form = $('#form-delete');
+            $form.attr('action', $form.attr('data-url') +'/'+ id);
+            $form.submit();
+        }
+    })
+})
+</script>
